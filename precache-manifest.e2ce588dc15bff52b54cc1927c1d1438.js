41be851b35792ce66c47e23199a95b04253ad155
kunal <kpandey@scu.edu>
2020-08-20 05:40:17 -0700

    Updates

---
+++ precache-manifest.e2ce588dc15bff52b54cc1927c1d1438.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3805ff4c60f4b2dfbec534654ba849e8",
+    "url": "/PortfolioSite/index.html"
+  },
+  {
+    "revision": "ec9b8bd5091c744121f0",
+    "url": "/PortfolioSite/static/css/main.4af15143.chunk.css"
+  },
+  {
+    "revision": "cdcc0e1ec38d1c849de8",
+    "url": "/PortfolioSite/static/js/2.595688b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PortfolioSite/static/js/2.595688b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9b8bd5091c744121f0",
+    "url": "/PortfolioSite/static/js/main.101425d0.chunk.js"
+  },
+  {
+    "revision": "8a7d83b324b9ca99538a",
+    "url": "/PortfolioSite/static/js/runtime-main.544905d9.js"
+  },
+  {
+    "revision": "bed86dca52731bf97fbf5bb38464517b",
+    "url": "/PortfolioSite/static/media/IronEng.bed86dca.png"
+  },
+  {
+    "revision": "3ff965b647aa21770daa17bfaac79fd8",
+    "url": "/PortfolioSite/static/media/bootstrap.3ff965b6.svg"
+  },
+  {
+    "revision": "a86c69bbc620594e6f62b4c2bd737b09",
+    "url": "/PortfolioSite/static/media/ec2.a86c69bb.svg"
+  },
+  {
+    "revision": "aaec9219d68861e16f79211b86d4df6d",
+    "url": "/PortfolioSite/static/media/git.aaec9219.svg"
+  },
+  {
+    "revision": "1750184fe9e4b574eefdbf19dec0ceae",
+    "url": "/PortfolioSite/static/media/hibernate.1750184f.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/PortfolioSite/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/PortfolioSite/static/media/nodejs.89ccb090.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/PortfolioSite/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "f89c76509624697512ee8c42abc5f2ca",
+    "url": "/PortfolioSite/static/media/sanfran.f89c7650.jpg"
+  },
+  {
+    "revision": "81c11a33ad6f12426165789ac78cb13e",
+    "url": "/PortfolioSite/static/media/secondpart.81c11a33.mov"
+  },
+  {
+    "revision": "5e9d46e546278ce2bcc03cfb590a2915",
+    "url": "/PortfolioSite/static/media/spring.5e9d46e5.svg"
+  },
+  {
+    "revision": "3323dd0202edcaf8d4392e994668959c",
+    "url": "/PortfolioSite/static/media/tailwind.3323dd02.svg"
+  },
+  {
+    "revision": "09fd1fa763be0415b4f13777acd62259",
+    "url": "/PortfolioSite/static/media/techabstract2.09fd1fa7.png"
+  },
+  {
+    "revision": "e69d844b6e8181f0837b80394d71583b",
+    "url": "/PortfolioSite/static/media/wip.e69d844b.gif"
+  }
+]);
